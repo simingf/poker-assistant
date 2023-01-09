fac7f23ca0eb6af079a86c8d8b3c052738351337
SimingFeng <simingrox@gmail.com>
2023-01-09 16:59:55 +0000

    finished probs for 3 4 5

---
+++ play.py
@@ -0,0 +1,90 @@
+from util import print_probs_0, print_probs_3, print_probs_4, print_probs_5
+
+def play_round():
+    num_players = get_num_players()
+    if num_players == 'q' or num_players == 'n':
+        return num_players
+    player_hand = get_cards("Enter your hand (2 cards)", 2)
+    if player_hand == 'q' or player_hand == 'n':
+        return player_hand
+    river = []
+    print_state(player_hand, river)
+    #print starting probs
+    new_cards = get_cards("Enter the first 3 cards of the river", 3)
+    if new_cards == 'q' or new_cards == 'n':
+        return new_cards
+    river += new_cards
+    print_state(player_hand, river)
+    print_probs_3(player_hand, river)
+    new_cards = get_cards("Enter the 4th card of the river", 1)
+    if new_cards == 'q' or new_cards == 'n':
+        return new_cards
+    river += new_cards
+    print_state(player_hand, river)
+    print_probs_4(player_hand, river)
+    new_cards = get_cards("Enter the last card of the river", 1)
+    if new_cards == 'q' or new_cards == 'n':
+        return new_cards
+    river += new_cards
+    print_state(player_hand, river)
+    print_probs_5(player_hand, river)
+    return 'n'
+
+def get_num_players():
+    num_players = input("How many players? (2-10) \n")
+    if num_players == "q":
+        return "q"
+    elif num_players == "n":
+        return "n"
+    else:
+        try:
+            num_players = int(num_players)
+            if num_players < 2 or num_players > 10:
+                print("Invalid input")
+                return get_num_players()
+            return num_players
+        except ValueError:
+            print("Invalid input")
+            return get_num_players()
+
+def get_cards(prompt, num_cards):
+    player_hand = input(prompt + " (\"h\" for help) \n")
+    if player_hand == "q":
+        return "q"
+    elif player_hand == "n":
+        return "n"
+    elif player_hand == "h":
+        print("Card numbers are 23456789TJQKA")
+        print("Card suits are cdhs")
+        print("Example: \"As Kd\"")
+        return get_cards(prompt, num_cards)
+    else:
+        try:
+            player_hand = str(player_hand).split()
+            if len(player_hand) != num_cards:
+                print("Invalid input")
+                return get_cards(prompt, num_cards)
+            for card in player_hand:
+                if len(card) != 2 or card[0] not in "23456789TJQKA" or card[1] not in "cdhs":
+                    print("Invalid input")
+                    return get_cards(prompt, num_cards)
+            return player_hand
+        except ValueError:
+            print("Invalid input")
+            return get_cards(prompt, num_cards)
+
+def print_state(hand, river):
+    print("Your hand: " + str(hand))
+    print("River: " + str(river))
+
+if __name__ == "__main__":
+    print("Type \"n\" to start a new round")
+    print("Type \"q\" to quit")
+    total_rounds = 1
+    while True:
+        print("Round " + str(total_rounds))
+        res = play_round()
+        if res == "q":
+            break
+        elif res == "n":
+            total_rounds += 1
---
+++ util.py
@@ -0,0 +1,156 @@
+import itertools
+
+hands = {0: "High Card", 1: "Pair", 2: "Two Pair", 3: "Three of a Kind", 4: "Straight", 5: "Flush", 6: "Full House", 7: "Four of a Kind", 8: "Straight Flush", 9: "Royal Flush"}
+
+def print_probs_3(hand, river):
+    probs = get_probs_3(hand, river)
+    for i in range(10):
+        if probs[i] == 0:
+            continue
+        else:
+            print(hands[i] + ": " + str(round(probs[i] * 100, 2)) + "%")
+    print()
+
+def print_probs_4(hand, river):
+    probs = get_probs_4(hand, river)
+    for i in range(10):
+        if probs[i] == 0:
+            continue
+        else:
+            print(hands[i] + ": " + str(round(probs[i] * 100, 2)) + "%")
+    print()
+
+def print_probs_5(hand, river):
+    probs = get_probs_5(hand, river)
+    for i in range(10):
+        if probs[i] == 0:
+            continue
+        else:
+            print(hands[i] + ": " + str(round(probs[i] * 100, 2)) + "%")
+    print()
+
+def get_probs_3(hand, river):
+    count = 0
+    hands_count = {}
+    known_cards = hand + river
+    for i in "23456789TJQKA":
+        for i_s in "cdhs":
+            for j in "23456789TJQKA":
+                for j_s in "cdhs":
+                    if i + i_s not in hand and i + i_s not in river and j + j_s not in hand and j + j_s not in river and i + i_s != j + j_s:
+                        count += 1
+                        cards = known_cards + [i + i_s, j + j_s]
+                        hands_count[best_hand(cards)] = hands_count.get(best_hand(cards), 0) + 1
+    probs = []
+    for i in range(10):
+        probs.append(hands_count.get(i, 0) / float(count))
+    return probs
+
+def get_probs_4(hand, river):
+    count = 0
+    hands_count = {}
+    known_cards = hand + river
+    for i in "23456789TJQKA":
+        for i_s in "cdhs":
+            if i + i_s not in hand and i + i_s not in river:
+                count += 1
+                cards = known_cards + [i + i_s]
+                hands_count[best_hand(cards)] = hands_count.get(best_hand(cards), 0) + 1
+    probs = []
+    for i in range(10):
+        probs.append(hands_count.get(i, 0) / float(count))
+    return probs
+
+def get_probs_5(hand, river):
+    cards = hand + river
+    index = best_hand(cards)
+    res = [0] * 10
+    res[index] = 1
+    return res
+
+def best_hand(hand):
+    assert(len(hand) == 7)
+
+    best = 0
+    for subset in itertools.combinations(hand, 5):
+        best = max(best, evaluate_hand(subset))
+    return best
+
+def evaluate_hand(hand):
+    assert(len(hand) == 5)
+
+    suits = [card[1] for card in hand]
+    flush = False
+    for suit in suits:
+        if suits.count(suit) == 5:
+            flush = True
+
+    straight = False
+    royal = False
+    numbers = [to_num(card[0]) for card in hand]
+    if 14 in numbers:
+        numbers += [1]
+    for i in range(1, 11):
+        if i in numbers and i+1 in numbers and i+2 in numbers and i+3 in numbers and i+4 in numbers:
+            straight = True
+            if i == 10:
+                royal = True
+    
+    if royal and flush and straight:
+        return 9
+    elif flush and straight:
+        return 8
+    
+    for card in numbers:
+        if numbers.count(card) == 4:
+            return 7
+        elif numbers.count(card) == 3:
+            for card2 in numbers:
+                if numbers.count(card2) == 2:
+                    return 6
+    
+    if flush:
+        return 5
+
+    if straight:
+        return 4
+    
+    for card in numbers:
+        if numbers.count(card) == 3:
+            return 3
+        elif numbers.count(card) == 2:
+            for card2 in numbers:
+                if numbers.count(card2) == 2 and card != card2:
+                    return 2
+            return 1
+    
+    return 0
+
+def to_num(card):
+    if card == "T":
+        return 10
+    elif card == "J":
+        return 11
+    elif card == "Q":
+        return 12
+    elif card == "K":
+        return 13
+    elif card == "A":
+        return 14
+    else:
+        return int(card)
+
+if __name__ == "__main__":
+    print(best_hand(["As", "Ks", "Qs", "Js", "Ts", "9s", "8s"]))
+    print(best_hand(["As", "2s", "3s", "4s", "5s", "9s", "8d"]))
+    print(best_hand(["As", "Ad", "Ah", "Ac", "6s", "6d", "8c"]))
+    print(best_hand(["As", "Ad", "Ah", "3d", "6s", "6d", "8c"]))
+    print(best_hand(["3h", "5h", "Ah", "Jh", "6h", "6d", "8c"]))
+    print(best_hand(["3h", "4d", "5h", "6c", "7d", "6d", "8c"]))
+    print(best_hand(["3h", "3d", "3c", "Jh", "5h", "6d", "8c"]))
+    print(best_hand(["3h", "3d", "7c", "Jh", "6h", "6d", "8c"]))
+    print(best_hand(["3h", "3d", "7c", "Jh", "6h", "9d", "8c"]))
+    print(best_hand(["2h", "3d", "7c", "Jh", "6h", "9d", "8c"]))
+
+    
+    
